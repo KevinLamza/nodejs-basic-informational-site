010fa759e37e41abf2e72e0776f2913494149fcc
Kevin Lamza <148631894+KevinLamza@users.noreply.github.com>
2025-01-07 14:43:20 +0100

    Add serving right file according to url

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
 #!/usr/bin/env node
+
+import http from 'http';
+import url from 'url';
+import fs from 'fs';
+
+http
+  .createServer(function (req, res) {
+    const query = url.parse(req.url, true);
+    let filename = '.' + query.pathname;
+    if (query.pathname === '/') {
+      filename = './index.html';
+    }
+    fs.readFile(filename, function (err, data) {
+      if (err) {
+        res.writeHead(404, { 'Content-type': 'text/html' });
+        // fs.readFile('./404.html', function (err, data) {
+        //   if (err) {
+        //     return res.end('Something went wrong');
+        //   }
+        //   res.write(data);
+        //   return res.end();
+        // });
+        return res.end('404 Not Found');
+      }
+      res.writeHead(200, { 'Content-type': 'text/html' });
+      res.write(data);
+      return res.end();
+    });
+  })
+  .listen(8080);
+
+// vielleicht irgendwas mit readFileSync?
